990ad21b9e7536f596ab5f5e5b967f43bf66e7c6
nicomencia <47212402+nicomencia@users.noreply.github.com>
2026-03-03 18:12:51 +0100

    Updated Hero.css

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -76,13 +76,13 @@ export default function Hero({ setCurrentView }) {
           <div className="hero-sobre-text">
             <h3 className="hero-sobre-titulo">Sobre mi...</h3>
             <p>Soy Paula Sastre Valverde, psicóloga sanitaria y personita curiosa siempre buscando la forma de conectar consigo misma, su creatividad, las demás y con el entorno en el que vive.</p>
+            <div className="hero-sobre-photo">
+              <img src="/sobreMi.jpg" alt="Paula Sastre" />
+            </div>
             <p>Mi enfoque terapéutico se basa en la integración de diferentes modelos y técnicas, lo cual me permite adaptarme a las necesidades de las personas a las que acompaño y ofrecerles las visiones más adecuadas a su momento vital.</p>
             <p>Dada mi diversa formación y experiencia, integro la perspectiva feminista e interseccional, reconociendo los malestares y opresiones sistemáticas que nos atraviesan y ofreciendo una mirada crítica sobre nuestro contexto y sus aprendizajes.</p>
             <p>Ofrezco un trabajo integral de cuerpo, mente y emociones, no dejando nada atrás.</p>
             <p>Recuerda que eres mucho más que tus problemas y en esto se basa mi intervención. Miraremos con respeto y ternura cada parte de tu ser y (re)conoceremos tus herramientas y fortalezas, mientras integramos aquello que busca su lugar.</p>
-          </div>
-          <div className="hero-sobre-photo">
-            <img src="/sobreMi.jpg" alt="Paula Sastre" />
           </div>
         </div>
         <div className="hero-sobre-bottom">
