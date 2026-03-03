ac7babba40107c11e7e598a6345febef154fc92c
nicomencia <47212402+nicomencia@users.noreply.github.com>
2026-03-03 18:58:59 +0100

    Updated Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -87,7 +87,7 @@ export default function Hero({ setCurrentView }) {
         </div>
         <div className="hero-sobre-bottom">
           <div className="hero-sobre-imagen">
-            <img src="/paula.jpg" alt="" />
+            <img src="/sobreMi2.jpg" alt="" />
           </div>
           <blockquote className="hero-sobre-cita">
             <p>Conozca todas las teorías, domine todas las técnicas, pero al tocar un alma humana sea apenas otra alma humana.</p>
