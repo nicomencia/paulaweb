8ece5147d5a6847f2768ba1bc9255cf74102560b
nicomencia <47212402+nicomencia@users.noreply.github.com>
2026-03-04 22:27:45 +0100

    Updated FormacionProfesionales.jsx

--- src/components/FormacionProfesionales.jsx
+++ src/components/FormacionProfesionales.jsx
@@ -9,7 +9,8 @@ export default function FormacionProfesionales({ onBack }) {
       </div>
 
       <div className="sp-texto-centrado">
-        <p>Si estás buscando una profesional que imparta pequeñas píldoras formativas sobre psicoeducación emocional, trauma, prevención del ASI, crisis vitales, gestión de conflictos y habilidades sociales, o el uso del arte como herramienta terapéutica, entre otros… ¡Soy tu persona!.</p>
+        <p>Si estás buscando una profesional que imparta pequeñas píldoras formativas sobre psicoeducación emocional, trauma, prevención del ASI, crisis vitales, gestión de conflictos y habilidades sociales, o el uso del arte como herramienta terapéutica, entre otros…</p>
+        <p>¡Soy tu persona!</p>
         <p>Cuéntame qué necesitas y juntas generaremos un espacio riguroso y vivencial, donde integro teoría y práctica desde una mirada integradora, sensible y adaptada al contexto y necesidades del grupo.</p>
       </div>
 
